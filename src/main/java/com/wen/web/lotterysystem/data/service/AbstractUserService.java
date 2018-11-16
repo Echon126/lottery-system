002198b74173524162b1ee2ae2ce22b7f93c7a8f
Echon126 <15620742723@163.com>
2018-11-16 15:45:28 +0800

    1。提交前段;

---
+++ src/main/java/com/wen/web/lotterysystem/data/service/AbstractUserService.java
@@ -0,0 +1,16 @@
+package com.wen.web.lotterysystem.data.service;
+
+
+import com.wen.web.lotterysystem.data.entity.UserDO;
+
+/**
+ * @author admin
+ * @date 2018-11-15 17:10
+ */
+public  abstract class AbstractUserService implements UserService{
+
+    @Override
+    public UserDO get(String id) {
+        return null;
+    }
+}
